968af584d78e76dc304debcaf57fdac2a34e9c0a
AndreLeBlanc <leblanc_112@hotmail.com>
2023-01-21 00:18:22 +0100

    comments

--- src/ArraySplit.purs
+++ src/ArraySplit.purs
@@ -1,11 +1,28 @@
-module ArraySplit where
+module ArraySplit
+  ( permutations
+  , group
+  , groups
+  , chop
+  , divvy
+  , tails
+  , splitAtElems
+  , splitAtElem
+  , splitAtSubList
+  , splitAtIndices
+  ) where
 
 import Prelude
 
-import Data.Array (length)
 import Data.Array as A
 import Data.Maybe (Maybe(..), maybe, fromMaybe)
 
+-- | Creates an `Array` containing all the permutations of an `Array`.
+-- |
+-- | ```purescript
+-- | permutations [1] = [1]
+-- | permutations [1, 2, 3] = [[1,2,3],[2,1,3],[2,3,1],[1,3,2],[3,1,2],[3,2,1]]
+-- | ```
+-- |
 permutations :: forall a. Array a -> Array (Array a)
 permutations [] = []
 permutations [ x ] = [ [ x ] ]
@@ -16,12 +33,27 @@ permutations xs = do
     Just x -> maybe [] pure (A.insertAt i x p)
     _ -> []
 
+-- | Splits an `Array` into subarrays of size n.
+-- |
+-- | ```purescript
+-- | group 1 [1,2,3] = [[1], [2], [3]]
+-- | group 2 [1, 2, 3, 4, 5] = [[1, 2],[3, 4],[5]]
+-- | ```
+-- |
 group :: forall a. Int -> Array a -> Array (Array a)
 group _ [] = []
 group n l
   | n > 0 = [ (A.take n l) ] <> ((group n (A.drop n l)))
   | otherwise = []
 
+-- | Splits an `Array` into subarrays of the sizes given by indicies array. 
+-- | Elements that don't fit in the subarrays are excluded.
+-- |
+-- | ```purescript
+-- | groups [1, 2] [1,2,3] = [[1], [2, 3]]
+-- | groups [2, 3] [1, 2, 3, 4, 5, 6] = [[1, 2],[3, 4, 5]]
+-- | ```
+-- |
 groups :: forall a. Array Int -> Array a -> Array (Array a)
 groups _ [] = []
 groups indicies xs =
@@ -39,14 +71,34 @@ chop f as =
     chop f as
       # A.cons el
 
+-- | Divides up an input array into a set of subarrays, according to 'n' and 'm'
+-- |  input specifications you provide. Each subarray will have 'n' items, and the
+-- |  start of each subarray will be offset by 'm' items from the previous one.
+-- | ```purescript
+-- | divvy 5 5 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]
+-- | tails [1] = []
+-- | ```
+-- |  In the case where a source array's trailing elements do no fill an entire
+-- |  subarray, those trailing elements will be dropped.
+--
+-- | ```purescript
+-- | divvy 5 2 [1..10] == [[1,2,3,4,5],[3,4,5,6,7],[5,6,7,8,9]]
+-- | ```
 divvy :: forall a. Int -> Int -> Array a -> Array (Array a)
 divvy _ _ [] = []
 divvy n m xs = A.filter (\ws -> (n == A.length ws)) choppedl
   where
   choppedl = chop (\x -> { el: A.take n x, as: A.drop m x }) xs
 
+-- | Creates an Array containing all the tails of a given array.
+-- |
+-- | ```purescript
+-- | tails [1,2,3,4,5,6] = [[6],[5,6],[4,5,6],[3,4,5,6],[2,3,4,5,6]]
+-- | tails [1] = []
+-- | ```
+-- |
 tails :: forall a. Array a -> Array (Array a)
-tails x | length x < 2 = []
+tails x | A.length x < 2 = []
 tails xs = go (pure (A.drop 1 xs)) (A.drop 1 xs)
   where
   go :: Array (Array a) -> Array a -> Array (Array a)
