92c7d2f25c79f7a82ab84bd329a4788a03d79940
AndreLeBlanc <leblanc_112@hotmail.com>
2023-01-20 23:27:00 +0100

    groups, splitAtIndicies all tested

--- src/ArraySplit.purs
+++ src/ArraySplit.purs
@@ -3,51 +3,85 @@ module ArraySplit where
 import Prelude
 
 import Data.Array (length)
-import Data.Array as Array
-import Data.Maybe (Maybe(..), maybe)
+import Data.Array as A
+import Data.Maybe (Maybe(..), maybe, fromMaybe)
 
 permutations :: forall a. Array a -> Array (Array a)
 permutations [] = []
 permutations [ x ] = [ [ x ] ]
 permutations xs = do
-  p <- permutations (Array.drop 1 xs)
-  i <- Array.range 0 ((Array.length xs) - 1)
-  case Array.head xs of
-    Just x -> maybe [] pure (Array.insertAt i x p)
+  p <- permutations (A.drop 1 xs)
+  i <- A.range 0 ((A.length xs) - 1)
+  case A.head xs of
+    Just x -> maybe [] pure (A.insertAt i x p)
     _ -> []
 
 group :: forall a. Int -> Array a -> Array (Array a)
 group _ [] = []
 group n l
-  | n > 0 = [ (Array.take n l) ] <> ((group n (Array.drop n l)))
+  | n > 0 = [ (A.take n l) ] <> ((group n (A.drop n l)))
   | otherwise = []
 
+groups :: forall a. Array Int -> Array a -> Array (Array a)
+groups [] _ = []
+groups _ [] = []
+groups indicies xs =
+  do
+    { head, tail } <- A.uncons indicies
+    [ A.take head xs ] <> groups tail (A.drop head xs) # pure
+    # fromMaybe []
 
---divvy 5 5 [1..20] == [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]     
---
+chop :: forall a b. (Array a -> { el :: b, as :: Array a }) -> Array a -> Array b
+chop _ [] = []
+chop f as =
+  let
+    { as, el } = f as
+  in
+    chop f as
+      # A.cons el
+
+divvy :: forall a. Int -> Int -> Array a -> Array (Array a)
+divvy _ _ [] = []
+divvy n m xs = A.filter (\ws -> (n == A.length ws)) choppedl
+  where
+  choppedl = chop (\x -> { el: A.take n x, as: A.drop m x }) xs
+
+tails :: forall a. Array a -> Array (Array a)
+tails x | length x < 2 = []
+tails xs = go (pure (A.drop 1 xs)) (A.drop 1 xs)
+  where
+  go :: Array (Array a) -> Array a -> Array (Array a)
+  go acc ys =
+    case A.drop 1 ys of
+      [] -> acc
+      rest -> go (A.cons rest acc) rest
+
 splitAtElems :: forall a. Ord a => Array a -> Array a -> Array (Array a)
 splitAtElems splitters xs =
   let
-    subs = Array.foldl
-      (\acc x -> if Array.elem x splitters then { head: [ x ], tail: acc.tail <> [ acc.head ] } else acc { head = Array.snoc acc.head x })
+    subs = A.foldl
+      (\acc x -> if A.elem x splitters then { head: [ x ], tail: acc.tail <> [ acc.head ] } else acc { head = A.snoc acc.head x })
       { head: [], tail: [] }
       xs
   in
     subs.tail <> [ subs.head ]
- 
+
 splitAtElem :: forall a. Ord a => a -> Array a -> Array (Array a)
-splitAtElem splitter xs = splitAtElems [splitter] xs
+splitAtElem splitter xs = splitAtElems [ splitter ] xs
 
---splitAtSubList do notation case [x c v]
+splitAtSubList :: forall a. Eq a => Array a -> Array a -> Array (Array a)
+splitAtSubList sub xs =
+  let
+    { h, t } = A.foldl (\acc x -> if A.elem x sub then { h: [], t: A.snoc acc.t acc.h } else acc { h = A.snoc acc.h x }) { h: [], t: [] } xs
+  in
+    A.cons h t
 
---split (onSublist "xyz") "aazbxyzcxd" == ["aazb","xyz","cxd"]
-
-tails :: forall a. Array a -> Array (Array a)
-tails x | length x < 2 = []
-tails xs = go (pure (Array.drop 1 xs)) (Array.drop 1 xs)
-  where
-  go :: Array (Array a) -> Array a -> Array (Array a)
-  go acc ys =
-    case Array.drop 1 ys of
-      [] -> acc
-      rest -> go (Array.cons rest acc) rest
+splitAtIndices :: forall a. Array Int -> Array a -> Array (Array a)
+splitAtIndices [] xs = [ xs ]
+splitAtIndices indicies xs =
+  do
+    { head, tail } <- A.uncons indicies
+    let newTail = A.foldl (\acc t -> A.cons (t - head) acc) [] tail
+    let { before, after } = A.splitAt head xs
+    A.cons before (splitAtIndices newTail after) # pure
+    # fromMaybe []
--- test/Main.purs
+++ test/Main.purs
@@ -2,17 +2,29 @@ module Test.Main where
 
 import Prelude
 
-import ArraySplit (group, splitAtElem, permutations, tails)
-import Control.Monad.List.Trans (uncons)
+import ArraySplit (group, groups, permutations, splitAtElem, splitAtIndices, tails)
 import Data.Array as A
-import Data.Maybe (fromMaybe)
-import Data.Set (fromFoldable, Set)
+import Data.Set (Set, fromFoldable, size)
 import Effect (Effect)
-import Test.QuickCheck (Result, (===))
+import Test.QuickCheck ((===))
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Main (runTest)
 import Test.Unit.QuickCheck (quickCheck)
+
+orderings :: Int -> Int
+orderings 0 = 0
+orderings 1 = 1
+orderings n = n * orderings (n - 1)
+
+splitTwoIndices :: forall a. Int -> Int -> Array a -> Array (Array a)
+splitTwoIndices i j xs =
+  A.cons (A.take j xs # A.drop i) [ A.drop (max i j) xs ]
+    # A.cons (A.take i xs)
+
+small :: Int -> Int
+small x | x > 0 = mod x 100
+small x = mod (-x) 100
 
 main ∷ Effect Unit
 main =
@@ -28,6 +40,8 @@ main =
         Assert.equal (permutations [ 1, 2, 3 ] # fromFoldable) $ fromFoldable [ [ 1, 2, 3 ], [ 1, 3, 2 ], [ 2, 1, 3 ], [ 2, 3, 1 ], [ 3, 1, 2 ], [ 3, 2, 1 ] ]
       test "DCODE" do
         Assert.equal (permutations [ "D", "C", "O", "D", "E" ] # fromFoldable) $ permsDCODE
+      test "quick permutations" do
+        quickCheck \(xs :: Array Char) -> (size $ fromFoldable $ permutations xs) === (orderings $ A.length xs)
 
     suite "group" do
       test "empty" do
@@ -42,10 +56,26 @@ main =
         Assert.equal (group 3 [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]) [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 10, 11, 12 ] ]
       test "group three uneven" do
         Assert.equal (group 3 [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ]) [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 10, 11, 12 ], [ 13 ] ]
-    
+      test "quick group" do
+        quickCheck \(xs :: Array Int) g -> (A.concat $ group g xs) === if 0 < g then xs else []
+
+    suite "groups" do
+      test "empty index, empty xs" do
+        Assert.equal (groups [] ([] :: Array Int)) $ []
+      test "empty index" do
+        Assert.equal (groups [] [ 1, 2, 3 ]) $ []
+      test "empty xs" do
+        Assert.equal (groups [ 1, 2, 3 ] ([] :: Array Int)) $ []
+      test "indicies 1,2,3, xs 1..9" do
+        Assert.equal (groups [ 1, 2, 3 ] [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]) $ [ [ 1 ], [ 2, 3 ], [ 4, 5, 6 ] ]
+      test "indicies 1 xs 1..9" do
+        Assert.equal (groups [ 1 ] [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]) $ [ [ 1 ] ]
+      test "indicies 1,5,12 xs 1..9" do
+        Assert.equal (groups [ 1, 5, 12 ] [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]) $ [ [ 1 ], [ 2, 3, 4, 5, 6 ], [ 7, 8, 9 ] ]
+
     suite "splitAtElem" do
       test "quickcheck" do
-        quickCheck (\(a :: Int)  b c->  splitAtElem a  (b <> [a] <> c) === [ b, [a] <> c])
+        quickCheck \(a :: Int) b c -> splitAtElem a (b <> [ a ] <> c) === [ b, [ a ] <> c ]
 
     suite "tails" do
       test "empty" do
@@ -54,10 +84,20 @@ main =
         Assert.equal (tails [ 1 ]) []
       test "two" do
         Assert.equal (tails [ "a", "b" ]) [ [ "b" ] ]
-      test "three" do
-        Assert.equal (tails [ "a", "b", "c" ]) [ [ "c" ], [ "b", "c" ] ]
-      test "ten" do
-        Assert.equal (tails [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ]) [ [ 0 ], [ 9, 0 ], [ 8, 9, 0 ], [ 7, 8, 9, 0 ], [ 6, 7, 8, 9, 0 ], [ 5, 6, 7, 8, 9, 0 ], [ 4, 5, 6, 7, 8, 9, 0 ], [ 3, 4, 5, 6, 7, 8, 9, 0 ], [ 2, 3, 4, 5, 6, 7, 8, 9, 0 ] ]
+      test "quickcheck tails" do
+        quickCheck
+          \(xs :: Array Int) ->
+            let
+              xs2 = xs <> [ 1, 2 ]
+            in
+              (tails xs2 # fromFoldable) ===
+                (fromFoldable $ map (\x -> A.reverse xs2 # A.take x # A.reverse) (A.range 1 (A.length xs + 1)))
+
+    suite "splitAtIndices" do
+      test "quick multi concat" do
+        quickCheck \index (xs :: Array Char) -> (splitAtIndices index xs # A.concat) === xs
+      test "quick multi two indexes" do
+        quickCheck \i j (xs :: Array Char) -> (splitAtIndices [ small i, small j ] xs) === splitTwoIndices (small i) (small j) xs
 
 permsDCODE :: Set (Array String)
 permsDCODE = fromFoldable
